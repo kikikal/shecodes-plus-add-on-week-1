0ef6d453d2e3432944808a5512e61ce5e1f5b168
kikikal <156766658+kikikal@users.noreply.github.com>
2024-03-16 19:12:28 +1100

    upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 function timeZoneAlert(event) {
-  let timeZone = "";
-  if (event.target.value === "paris") {
-    timeZone = `Europe/Paris`;
-  }
-  if (event.target.value === "tokyo") {
-    timeZone = "Asia/Tokyo";
-  }
-  if (event.target.value === "sydney") {
-    timeZone = "Australia/Sydney";
-  }
   alert(
     `it is ${moment
-      .tz(timeZone)
-      .format("dddd, MMMM D, YYYY h:mm A")} in ${timeZone}`
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A")} in ${event.target.value}`
   );
 }
 let city = document.querySelector("#cities");
